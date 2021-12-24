e3b4478a6d1ab8fa601c3090bfa77323bfc4a905
Nityasmit Mallick <mallicknityasmit2001@gmail.com>
2021-12-24 20:34:03 +0530

    Fetching Followers

---
+++ igs.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+import instaloader
+
+load_dotenv()
+
+username = os.environ.get('userid')
+password = os.environ.get('password')
+
+L = instaloader.Instaloader()
+
+
+L.login(username, password)
+
+profile = instaloader.Profile.from_username(L.context, r'ntx23')
+
+followersList = []
+
+for follower in profile.get_followers():
+    followersList.append(follower.username)
+
+print("Followers list obtained")
+print("Followers: " + str(len(followersList)))
